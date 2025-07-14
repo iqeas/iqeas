2b3da9aef27fcf2c0ba3a01e6aad28a4612cf5d5
Aromal <aromals161@gmail.com>
2025-07-14 13:52:20 +0530

    bug fix

--- backend/src/v1/routes/workflow.route.js
+++ backend/src/v1/routes/workflow.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import {
   createStage,
-  // uploadStageFiles,
+  uploadStageFiles,
   createDrawing,
-  // addDrawingStageLog,
+  addDrawingStageLog,
   getDrawingLogs,
   getStagesByProjectIdController,
   getStageDrawingsController,
@@ -14,19 +14,14 @@ const router = express.Router();
 
 router.post("/stages/:project_id", createStage);
 router.get("/stages/:project_id", getStagesByProjectIdController);
-router.get("/stages/drawings/:project_id/:stage_id", getStageDrawingsController);
-router.post(
-  "/stages/:id/files",
-  authenticateToken,
-  uploadStageFiles
+router.get(
+  "/stages/drawings/:project_id/:stage_id",
+  getStageDrawingsController
 );
+router.post("/stages/:id/files", authenticateToken, uploadStageFiles);
 
-router.post("/drawings",authenticateToken, createDrawing);
-router.post(
-  "/drawings/:id/logs",
-  authenticateToken,
-  addDrawingStageLog
-);
+router.post("/drawings", authenticateToken, createDrawing);
+router.post("/drawings/:id/logs", authenticateToken, addDrawingStageLog);
 router.get("/drawings/:id/logs", getDrawingLogs);
 
 export default router;
