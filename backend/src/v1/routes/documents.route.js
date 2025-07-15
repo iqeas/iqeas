0fe5d97b12ec6ffaecf7da5d11709570e497ef76
Muhammed Irshad <muhammedirshadismail0722@gmail.com>
2025-07-16 03:06:06 +0530

    save local changes

--- backend/src/v1/routes/documents.route.js
+++ backend/src/v1/routes/documents.route.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { getDocumentsHandler } from "../controllers/documents.controller.js";
-
+import {getDocumentsHandler} from '../controllers/documents.controller.js'
 const router = express.Router();
 
 router.get("/get-documents", getDocumentsHandler);
