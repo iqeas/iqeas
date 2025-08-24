c0bdef7c9a2a1ca867464996a5c7b14a2eca36fa
Muhammed Irshad <muhammed irshadismail0722@gmail.com>
2025-08-24 13:37:33 +0530

    invoice creation basic setup

--- backend/src/v1/lib/excel.js
+++ backend/src/v1/lib/excel.js
@@ -1,3 +1,36 @@
 export async function generateInvoiceExcel(invoiceData) {
   // write code here
 }
+
+
+/* Example usage (uncomment to test locally)
+  invoiceData: {
+      paymentApplicationNo: "",
+      paymentApplicationDate: "",
+      invoiceReference: "",
+      invoiceDate: "",
+      clientName: "",
+      clientAddress: "",
+      clientEmail: "",
+      clientGSTN: "",
+      projectNo: "",
+      projectName: "",
+      projectValue: "",
+      variationAmount: "",
+      billingPeriod: "",
+      totalTaxAmount: "",
+      totalPOAmount: "",
+      items: [
+        {
+          slNo: 1,
+          description: "",
+          previousProgress: "",
+          receivedAmount: "",
+          currentProgress: "",
+          currentAmount: "",
+          currentTax: "",
+        },
+      ],
+    },
+
+*/
